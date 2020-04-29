d9e751961a34d5b21bece44000083d562acc0332
omar <dinasil.omarbek@gmail.com>
2020-04-29 22:18:10 +0600

    finished main page

--- learn_kazakh-ui/src/main/java/kz/omar/ui/pages/MainLayoutFactory.java
+++ learn_kazakh-ui/src/main/java/kz/omar/ui/pages/MainLayoutFactory.java
@@ -3,10 +3,9 @@ package kz.omar.ui.pages;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.server.ThemeResource;
+import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.spring.annotation.SpringView;
-import com.vaadin.ui.Embedded;
-import com.vaadin.ui.Panel;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
 import kz.omar.ui.start.LearnKazakhMainUI;
 
 /**
@@ -21,15 +20,42 @@ public class MainLayoutFactory extends VerticalLayout implements View {
     
     private void addLayout() {
         setMargin(true);
+        setWidth("100%");
         
-        Panel panel = new Panel();
+        Panel borderPanel = new Panel();
+        Embedded border = new Embedded();
+        border.setSource(new ThemeResource("../../images/border.jpg"));
+        borderPanel.setContent(border);
+        addComponent(borderPanel);
         
-        Embedded logo = new Embedded();
-        logo.setSource(new ThemeResource("../../images/kazakh.png"));
+        Panel bottomPanel = new Panel();
+        HorizontalLayout bottomHL=new HorizontalLayout();
+        bottomHL.setSizeFull();
+    
+        Label welcomeLabel = new Label();
+        welcomeLabel.setValue("<h1>Добро пожаловать!</h1>" +
+                "<p>Здесь ты будешь учить все самое нужное и необходимое " +
+                "для понимания и освоения основ казахской речи." +
+                "<br>" +
+                "Чем больше слов ты изучишь, тем больше " +
+                "станет твой дракон!</p>");
+        welcomeLabel.setContentMode(ContentMode.HTML);
+        welcomeLabel.setStyleName("main-page");
+        bottomHL.addComponent(welcomeLabel);
+        bottomHL.setExpandRatio(welcomeLabel,2);
+        bottomHL.setComponentAlignment(welcomeLabel,Alignment.MIDDLE_RIGHT);
         
-        panel.setContent(logo);
         
-        addComponent(panel);
+        Embedded dragon = new Embedded();
+        dragon.setSource(new ThemeResource("../../images/asd.png"));
+        dragon.setWidth("300px");
+        dragon.setHeight("250px");
+        bottomHL.addComponent(dragon);
+        bottomHL.setComponentAlignment(dragon,Alignment.MIDDLE_LEFT);
+        bottomHL.setExpandRatio(dragon,1);
+        
+        bottomPanel.setContent(bottomHL);
+        addComponent(bottomPanel);
     }
     
     public void enter(ViewChangeListener.ViewChangeEvent viewChangeEvent) {
