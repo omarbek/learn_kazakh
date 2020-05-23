cbdcf8187c275e35ae8c94ca58440a38183eef0d
omar <dinasil.omarbek@gmail.com>
2020-05-24 00:00:35 +0600

    fixed alphabet from 2 to 12

---
+++ learn_kazakh-model/src/main/resources/db/changelog/omar/20200523/script_2.sql
@@ -0,0 +1,7 @@
+update alphabet set letter='Ғғ' where id=6;
+update alphabet set letter='Дд' where id=7;
+update alphabet set letter='Ее' where id=8;
+update alphabet set letter='Ёё' where id=9;
+update alphabet set letter='Жж' where id=10;
+update alphabet set letter='Зз' where id=11;
+update alphabet set letter='Ии' where id=12;
