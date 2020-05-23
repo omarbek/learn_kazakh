a1bca206f4145b06bfc17f9398b7de911cefb50e
omar <dinasil.omarbek@gmail.com>
2020-05-23 18:48:56 +0600

    refactored repository

--- learn_kazakh-repository/src/main/java/kz/omar/repository/alphabet/AlphabetRepository.java
+++ learn_kazakh-repository/src/main/java/kz/omar/repository/alphabet/AlphabetRepository.java
@@ -1,10 +1,8 @@
 package kz.omar.repository.alphabet;
 
 import kz.omar.model.entity.Alphabet;
-import kz.omar.model.entity.AlphabetMedia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,7 +13,7 @@ import java.util.List;
  * @project learn_kazakh
  */
 @Repository
-public interface AlphabetRepository extends JpaRepository<Alphabet,Integer> {
+public interface AlphabetRepository extends JpaRepository<Alphabet, Integer> {
     
     @Query("select a from Alphabet a")
     List<Alphabet> getAlphabet();
