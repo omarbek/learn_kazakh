8c5cf4e77eb64b0ed545e894a919d2efe543899d
omar <dinasil.omarbek@gmail.com>
2020-05-24 13:58:51 +0600

    fixed alphabet from 13 to 22

---
+++ learn_kazakh-model/src/main/resources/db/changelog/omar/20200524/script_1.sql
@@ -0,0 +1,10 @@
+insert into alphabet values (13, 'Йй');
+insert into alphabet values (14, 'Кк');
+insert into alphabet values (15, 'Ққ');
+insert into alphabet values (16, 'Лл');
+insert into alphabet values (17, 'Мм');
+insert into alphabet values (18, 'Нн');
+insert into alphabet values (19, 'Ңң');
+insert into alphabet values (20, 'Оо');
+insert into alphabet values (21, 'Өө');
+insert into alphabet values (22, 'Пп');
