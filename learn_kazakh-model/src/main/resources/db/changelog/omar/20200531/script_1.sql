f2570cf2177b09af05e5b284eb4514b09dfb0e1d
omar <dinasil.omarbek@gmail.com>
2020-06-06 10:11:26 +0600

    finished alphabet from 23 to 32

---
+++ learn_kazakh-model/src/main/resources/db/changelog/omar/20200531/script_1.sql
@@ -0,0 +1,10 @@
+insert into alphabet values (23, 'Рр');
+insert into alphabet values (24, 'Сс');
+insert into alphabet values (25, 'Тт');
+insert into alphabet values (26, 'Уу');
+insert into alphabet values (27, 'Ұұ');
+insert into alphabet values (28, 'Үү');
+insert into alphabet values (29, 'Фф');
+insert into alphabet values (30, 'Хх');
+insert into alphabet values (31, 'Һһ');
+insert into alphabet values (32, 'Цц');
---
+++ learn_kazakh-model/src/main/resources/db/changelog/omar/20200606/script_1.sql
@@ -0,0 +1,9 @@
+update alphabet_media set alphabet_id=24 where id in (70,71,72);
+update alphabet_media set alphabet_id=25 where id in (73,74,75);
+update alphabet_media set alphabet_id=26 where id in (76,77,78);
+update alphabet_media set alphabet_id=27 where id in (79,80,81);
+update alphabet_media set alphabet_id=28 where id in (82,83,84);
+update alphabet_media set alphabet_id=29 where id in (85,86,87);
+update alphabet_media set alphabet_id=30 where id in (88,89,90);
+update alphabet_media set alphabet_id=31 where id in (91,92,93);
+update alphabet_media set alphabet_id=32 where id in (94,95,96);
